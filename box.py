5d6b4c2dd503985244f98cb6d7b390c353658cc5
Simon David Pratt <me@SimonDavidPratt.com>
2011-07-19 22:07:48 -0400

    box size defaults to 15

--- box.py
+++ box.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
-BOX_SIZE=15
+import sys
+BOX_SIZE=15 if (len(sys.argv) < 2) else int(sys.argv[1])
 
 def inner(line,count):
     if line > 0:
-        toReturn = '* X' + (' '*count) + 'X' + (' '*line) + 'X' + (' '*count) + 'X *\n'
-        toReturn += inner(line-2,count+1)
-        toReturn += '* X' + (' '*count) + 'X' + (' '*line) + 'X' + (' '*count) + 'X *\n'
+        toReturn = '* X' + (' '*count) + 'X' + (' '*line) + 'X' + (' '*count) + 'X *\n' + inner(line-2,count+1) + '* X' + (' '*count) + 'X' + (' '*line) + 'X' + (' '*count) + 'X *\n'
     else:
         toReturn = '* X' + (' '*count) + 'X' + (' '*count) + 'X *\n'
     return toReturn  
@@ -13,13 +12,13 @@ def inner(line,count):
 toPrint = ''
 for i in range(0,BOX_SIZE):
     toPrint += '*'
-toPrint += '\n* '
+toPrint += '\n*' + (' '*(BOX_SIZE-2)) + '*\n* '
 for i in range(0,BOX_SIZE-4):
     toPrint += 'X'
 toPrint += ' *\n' + inner(BOX_SIZE-8,0) + '* '
 for i in range(0,BOX_SIZE-4):
     toPrint += 'X'
-toPrint += ' *\n'
+toPrint += ' *\n*' + (' '*(BOX_SIZE-2)) + '*\n'
 for i in range(0,BOX_SIZE):
     toPrint += '*'
 print toPrint
